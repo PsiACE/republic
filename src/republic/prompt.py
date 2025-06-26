ae4ae835a972949e75ab793a059420de1ca1d3a0
Chojan Shang <chojan.shang@vesoft.com>
2025-06-26 16:33:59 +0800

    feat: add support for prompt

--- src/republic/prompt.py
+++ src/republic/prompt.py
@@ -4,5 +4,7 @@ try:
     importlib.util.find_spec("republic_prompt")
     from republic_prompt import *  # noqa: F403, F401
 except ImportError:
-    print("`republic-prompt` is not installed. Please add it or add `republic[prompt]` to your project dependencies.")
+    print(
+        "`republic-prompt` is not installed. Please add it or add `republic[prompt]` to your project dependencies."
+    )
     exit(1)
